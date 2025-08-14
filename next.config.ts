df5c7308ee2a4f91b4ebf197c4d63a98d92e6bef
Mediteran2910 <marindonadini00@gmail.com>
2025-08-14 17:00:50 +0200

    adding webpack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import path from "path";
+import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack(config: Configuration) {
+    if (!config.resolve) config.resolve = {};
+    if (!config.resolve.alias) config.resolve.alias = {};
+    const alias = config.resolve.alias as { [key: string]: string };
+    alias["@"] = path.resolve(__dirname, "src");
+
+    return config;
+  },
 };
 
 export default nextConfig;
