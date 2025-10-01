41689689892766bf3421ccf9c23147c554d30904
Mediteran2910 <marindonadini00@gmail.com>
2025-10-01 21:08:41 +0200

    changing the logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,25 @@
 import MobileNav from "./MobileNav";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="w-full relative z-20 border-b border-gray-600 bg-gradient-to-r from-[#0D0D0D] via-[#171717] to-[#0D0D0D]">
-      <nav className="w-full px-8 lg:px-16 flex items-center justify-between py-4">
-        <div className="flex items-center">
-          <div className="border border-gray-600 text-gray-300 px-4 py-2 rounded-lg font-bold text-lg">
-            VPN-LOGO
-          </div>
-        </div>
+    <header className="w-full relative flex flex-col items-center justify-center z-20 border-b border-gray-600 bg-gradient-to-r from-[#0D0D0D] via-[#171717] to-[#0D0D0D]">
+      <nav className="w-full lg:w-[75%] pl-2 pr-8 lg:px-16 flex items-center justify-between py-4">
+        <Image
+          src="/logo-wide.png"
+          width={100}
+          height={100}
+          alt="company logo, wind icon and company name"
+          className="w-[150px] hidden lg:block"
+        ></Image>
+        <Image
+          src="/logo-small.png"
+          width={100}
+          height={100}
+          alt="company logo, wind icon"
+          className="w-[75px] lg:hidden"
+        ></Image>
 
         <div className="hidden lg:flex items-center space-x-8">
           <Link
